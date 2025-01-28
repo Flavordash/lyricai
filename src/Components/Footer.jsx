f3f4d6405f80930f9fac2b8648eabf4f347f069b
Kwanhoon Lee <back7930@gmail.com>
2025-01-28 13:27:30 -0500

    new

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px', backgroundColor: '#f8f9fa' }}>
-      <p style={{ margin: 0 }}>&copy; 2024 LYRICAi Made By Skiijump</p>
-    </footer>
+    <footer
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '50px',
+      backgroundColor: '#f8f9fa',
+    }}
+  >
+    <p style={{ margin: 0, color: 'black' }}>
+      &copy; 2024 LYRICAi Made By Skiijump
+    </p>
+  </footer>
   );
 }
 
